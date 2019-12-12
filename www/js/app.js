3097818eea717591b12169c3b4a8a4c9bf8b9fad
unknown <mdsalman@outlook.com>
2019-12-13 00:51:35 +0530

    Initial Angular Setup

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 var WMCWebApp = angular.module('WMCWebApp', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
-WMCWebApp.controller('PhoneListController', function PhoneListController($scope) {
+WMCWebApp.controller('WMCHomeController', function PhoneListController($scope) {
   $scope.phones = [
     {
       name: 'Nexus S',
@@ -15,4 +15,34 @@ WMCWebApp.controller('PhoneListController', function PhoneListController($scope)
       snippet: 'The Next, Next Generation tablet.'
     }
   ];
+
+  $scope.homePageData = {
+    heading : "WMC Autotrade",
+    heading2 : "Company",
+    caption : "Section 1.1. Home Page - Has some intro details about the company Need data from the WMC folks which can be added here. This is in Home page",
+    services : [
+      {
+        service_heading : "Trust worthy",
+        service_description : "We are a brand you can trust",
+        service_img : "img/features/f-icon1.png"
+      }, {
+        service_heading : "Best Price",
+        service_description : "Competitive price for your purchase",
+        service_img : "img/features/f-icon2.png"
+      }, {
+        service_heading : "Dedicated Customer Support",
+        service_description : "Super responsive customer services",
+        service_img : "img/features/f-icon3.png"
+      }, {
+        service_heading : "Easy Payment Options",
+        service_description : "We facilitate reliable payments",
+        service_img : "img/features/f-icon4.png"
+      }      
+    ]
+  };
+
+  $scope.exclusiveStocks = {
+
+  };
+
 });
